c6d78f8909dee4eb7de0cd76b9487e135845f077
Ritika Sharma <69035863+ritikaasharma@users.noreply.github.com>
2021-02-21 09:50:19 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 	 $dbhost = "localhost";
-	 $dbuser = "id16206615_ritika";
-	 $dbpass = "cQG-32=9-DZ\w&&A"; 
-	 $db = "bankingsystem";
+	 $dbuser = "";
+	 $dbpass = ""; 
+	 $db = "";
 	 $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 ?>	
